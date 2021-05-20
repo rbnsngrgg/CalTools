8617b0b5f4f2e5105e862ab098eece82f48d6e94
Gregg Robinson <grobinson@perceptron.com>
2021-05-20 09:27:39 -0400

    Removed config sections: "Report_Template", "Theme", and "TempFilesDir". (#84)

--- CalTools WPF/ObjectClasses/CTConfig.cs
+++ CalTools WPF/ObjectClasses/CTConfig.cs
@@ -12,12 +12,9 @@ namespace CalTools_WPF
         public string DbName { get; set; }
         public string DbPath { get; set; }
         public string ListDir { get; set; }
-        public string TempFilesDir { get; set; }
         public string ItemScansDir { get; set; }
-        public string Theme { get; set; }
         public int MarkDueDays { get; set; }
         public string CerificateFileName { get; set; }
-        public Dictionary<string, string> ReportCells { get; set; } = new Dictionary<string, string>();
         public List<string> Folders { get; set; } = new List<string>();
         public List<string> Procedures { get; set; } = new List<string>();
 
@@ -26,27 +23,12 @@ namespace CalTools_WPF
             try
             {
                 //Generate file and write first lines
-                string[] lines = { "<CalTools_Config Theme = \"Light\" Folders = \"PRODUCTION EQUIPMENT,ENGINEERING EQUIPMENT,QUALITY EQUIPMENT,Ref Only,Removed from Service\"" +
+                string[] lines = { "<CalTools_Config Folders = \"PRODUCTION EQUIPMENT,ENGINEERING EQUIPMENT,QUALITY EQUIPMENT,Ref Only,Removed from Service\"" +
                         " Procedures = \"019-0065\">",
                         "\t<Database DbName = \"Test Equipment Calibration List.db\"/>",
                         "\t<Directories ListDir = \"\\\\artemis\\Hardware Development Projects\\Manufacturing Engineering\\Test Equipment\" " +
-                        "TempFilesDir = \"\\\\artemis\\Hardware Development Projects\\Manufacturing Engineering\\Test Equipment\\Template Files\" " +
                         "ItemScansDir = \"\\\\artemis\\Hardware Development Projects\\Manufacturing Engineering\\Test Equipment\\Item Scans\"/>",
                         "\t<DueItems MarkDueDays = \"30\"/>",
-                        "\t<Report_Template " +
-                        "CertificateFileName = \"ReportTemplate.xlsx\" " +
-                        "ManufacturerCell = \"C5\" " +
-                        "ModelCell = \"C6\" " +
-                        "SerialNumberCell = \"C7\" " +
-                        "DescriptionCell = \"C8\" " +
-                        "CalibrationCell = \"J12\" " +
-                        "VerificationCell = \"J13\" " +
-                        "CalibrationDateCell = \"D18\" " +
-                        "OperationDateCell = \"D19\" " +
-                        "DueDateCell = \"D20\" " +
-                        "ProcedureCell = \"I18\" " +
-                        "LocationCell = \"I19\" " +
-                        "CertDateCell = \"J42\"/>",
                     "</CalTools_Config>" };
                 File.WriteAllLines(configPath, lines);
                 return true;
@@ -68,34 +50,22 @@ namespace CalTools_WPF
             {
                 XmlDocument config = new XmlDocument();
                 config.Load(configPath);
-                Folders.AddRange(config.LastChild.Attributes[1].Value.Split(","));
-                Procedures.AddRange(config.LastChild.Attributes[2].Value.Split(","));
-                Theme = config.LastChild.Attributes[0].Value;
+                if(config.LastChild.Attributes[0].Name == "Theme") { throw new System.Exception("Old config version"); }
+                Folders.AddRange(config.LastChild.Attributes[0].Value.Split(","));
+                Procedures.AddRange(config.LastChild.Attributes[1].Value.Split(","));
                 DbName = config.LastChild.ChildNodes[0].Attributes[0].Value;
                 ListDir = config.LastChild.ChildNodes[1].Attributes[0].Value;
-                TempFilesDir = config.LastChild.ChildNodes[1].Attributes[1].Value;
-                ItemScansDir = config.LastChild.ChildNodes[1].Attributes[2].Value;
+                ItemScansDir = config.LastChild.ChildNodes[1].Attributes[1].Value;
                 MarkDueDays = int.Parse(config.LastChild.ChildNodes[2].Attributes[0].Value);
-
-                ReportCells.Add("CerificateFileName", config.LastChild.ChildNodes[3].Attributes[0].Value);
-                ReportCells.Add("ManufacturerCell", config.LastChild.ChildNodes[3].Attributes[1].Value);
-                ReportCells.Add("ModelCell", config.LastChild.ChildNodes[3].Attributes[2].Value);
-                ReportCells.Add("SerialNumberCell", config.LastChild.ChildNodes[3].Attributes[3].Value);
-                ReportCells.Add("DescriptionCell", config.LastChild.ChildNodes[3].Attributes[4].Value);
-                ReportCells.Add("CalibrationCell", config.LastChild.ChildNodes[3].Attributes[5].Value);
-                ReportCells.Add("VerificationCell", config.LastChild.ChildNodes[3].Attributes[6].Value);
-                ReportCells.Add("CalibrationDateCell", config.LastChild.ChildNodes[3].Attributes[7].Value);
-                ReportCells.Add("OperationDateCell", config.LastChild.ChildNodes[3].Attributes[8].Value);
-                ReportCells.Add("DueDateCell", config.LastChild.ChildNodes[3].Attributes[9].Value);
-                ReportCells.Add("ProcedureCell", config.LastChild.ChildNodes[3].Attributes[10].Value);
-                ReportCells.Add("LocationCell", config.LastChild.ChildNodes[3].Attributes[11].Value);
-                ReportCells.Add("CertDateCell", config.LastChild.ChildNodes[3].Attributes[12].Value);
 
                 DbPath = Path.Combine(ListDir, DbName);
             }
             catch (System.Exception ex)
             {
-                MessageBox.Show($"Error loading config file: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show($"Error loading config file: {ex.Message}. " +
+                    $"The config may be incompatible with this version of CalTools. " +
+                    $"Rename or remove the config, then restart CalTools.",
+                    "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
